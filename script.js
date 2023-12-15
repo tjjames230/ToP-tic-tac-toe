680798d6be2d42c6b16dedc1c127ed38be76696e
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-12-15 14:11:23 -0600

    starting the player function/objects

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const Gameboard = () => {
-  const board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+const startScreen = (() => {
+  // select one or two players
+})();
 
-  const getBoard = () => {
-    return board;
+const gameboard = (() => {
+  // this will have everything related specifically with the gameboard
+})();
+
+const players = (() => {
+  // object containing both players, human or AI etc, X or O, turn, etc.
+  function createPlayers(player) {
+    return {
+      name: player,
+    };
+  }
+
+  return {
+    playerOne: createPlayers("p1"),
+    playerTwo: createPlayers("p2"),
   };
-};
+})();
